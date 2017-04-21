9fab1d5da3d0831b5a32e91d71a8de8c8d4fc373
Matei Marina <i.marina.16@aberdeen.ac.uk>
2017-04-21 13:59:16 +0100

    Finished creating conversations and adding send message link to profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -13,13 +13,18 @@ Rails.application.routes.draw do
   resources :rooms
   resources :photos
 
-  resources :room do
+  resources :rooms do
     resources :reservations, only: [:create]
+  end
+
+  resources :conversations, only: [:index, :create] do
+    resources :messages, only: [:index, :create]
   end
 
   get '/preload' => 'reservations#preload'
   get '/preview' => 'reservations#preview'
   get '/your_trips' => 'reservations#your_trips'
   get '/your_reservations' => 'reservations#your_reservations'
+  get '/conversations' => 'conversations#index'
 
 end
